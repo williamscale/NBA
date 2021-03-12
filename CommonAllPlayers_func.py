60685be8703ce84b12ce26af59bb79e9eec7d6f9
williamscale <calerwilliams@gmail.com>
2021-03-12 10:07:57 -0700

    Added optional team(s) argument.

--- CommonAllPlayers_func.py
+++ CommonAllPlayers_func.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import json
 from nba_api.stats.endpoints import commonallplayers
+import time
 
-def create_currentplayers_lists():
+def create_currentplayers_lists(teams = []):
 
 	response = commonallplayers.CommonAllPlayers(
 			is_only_current_season = 1)
+
+	time.sleep(0.1)
 
 	content = json.loads(response.get_json())
 
@@ -20,14 +23,42 @@ def create_currentplayers_lists():
 
 	currentplayers_df.columns = headers
 
-	currentplayer_id_list = currentplayers_df['PERSON_ID'].tolist()
-	currentplayer_list = currentplayers_df['DISPLAY_FIRST_LAST'].tolist()
+	if teams == []:
+
+		currentplayer_id_list = currentplayers_df['PERSON_ID'].tolist()
+
+		currentplayer_list = currentplayers_df['DISPLAY_FIRST_LAST'].tolist()
+
+	elif type(teams) == str:
+
+		currentplayer_id_team = currentplayers_df.loc[currentplayers_df['TEAM_NAME'] == teams]
+
+		currentplayer_id_list = currentplayer_id_team['PERSON_ID'].tolist()
+
+		currentplayer_list = currentplayer_id_team['DISPLAY_FIRST_LAST'].tolist()
+
+	elif teams:
+
+		currentplayer_id_lists = []
+		currentplayer_lists = []
+
+		for team in teams:
+
+			currentplayer_id_team = currentplayers_df.loc[currentplayers_df['TEAM_NAME'] == team]
+
+			currentplayer_id_lists.append(currentplayer_id_team['PERSON_ID'].tolist())
+			currentplayer_id_list = [player for team_list in currentplayer_id_lists for player in team_list]
+
+			currentplayer_lists.append(currentplayer_id_team['DISPLAY_FIRST_LAST'].tolist())
+			currentplayer_list = [player for team_list in currentplayer_lists for player in team_list]
 
 	return currentplayer_id_list, currentplayer_list
 
 def get_playerid(player_name):
 
 	response = commonallplayers.CommonAllPlayers()
+
+	time.sleep(0.5)
 
 	content = json.loads(response.get_json())
 
@@ -50,7 +81,5 @@ def get_playerid(player_name):
 if __name__ == '__main__':
 
 	player_name = 'Tony Parker'
-	x = create_currentplayers_lists()
-	y = get_playerid(player_name)
-
-	print(y)
+	x, y = create_currentplayers_lists(teams = ['Magic', 'Spurs'])
+	z = get_playerid(player_name)
