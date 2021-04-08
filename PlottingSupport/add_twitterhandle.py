3e12e548cc8e8f0f8fb09b16850fa302413ac6cf
williamscale <calerwilliams@gmail.com>
2021-04-08 14:04:49 -0600

    Initial commit.

---
+++ PlottingSupport/add_twitterhandle.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+def add_twitterhandle(fig_num):
+
+	# add twitter handle
+	plt.text(x = 100,
+		y = 400,
+		s = '\u0040cale_williams',
+		color = 'white',
+		fontstyle = 'italic'
+		)
