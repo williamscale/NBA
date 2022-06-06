9718784bd4823457ea8fdd2be4db57800c0c3fb0
williamscale <calerwilliams@gmail.com>
2022-06-06 18:07:13 -0500

    Covered corner case for players that didn't make playoffs.

--- R/Scrapers/bballref_player.R
+++ R/Scrapers/bballref_player.R
@@ -1,27 +1,52 @@
 
-player_bballref <- function(player_url, player_table = 1) {
+player.bballref <- function(player.url, player.table = 1) {
   
   library(rvest)
   
   # player_table Reference
   # 1: Per Game, Regular Season
-  # 2: Per Game, Playoffs
+  # 2: Per Game, Playoffs (unless they never made playoffs)
   # 3: Totals, Regular Season
   # 4: Totals, Playoffs
   # 5: Advanced, Regular Season
   # 6: Advanced, Playoffs
   
-  player_pergame <- player_url %>%
+  player.stats <- player.url %>%
     read_html() %>%
-    html_table() %>%
-    .[[player_table]]
+    html_table()
+  # %>%
+  #   .[[player_table]]
   
-  return (player_pergame)
+  if ((length(player.stats) == 3) & ((player.table == 2) | (player.table == 4) | (player.table == 4))) {
+
+    print('Player did not make playoffs.')
+
+  } else if ((length(player.stats) == 3) & (player.table == 3)) {
+
+    player.stats <- player.stats %>%
+      .[[2]]
+
+  } else if ((length(player.stats) == 3) & (player.table == 5)) {
+
+    player.stats <- player.stats %>%
+      .[[3]]
+
+  } else {
+
+    player.stats <- player.stats %>%
+      .[[player.table]]
+  }
+  
+  return (player.stats)
   
 }
 
+
+
 # TESTING -----------------------------------------------------------------
 
-x <- player_bballref('https://www.basketball-reference.com/players/a/antetgi01.html', 1)
-x$Season
-x[1, 'G']
+# x <- player.bballref('https://www.basketball-reference.com/players/m/murrade01.html', 5)
+# x
+# x[5, 1]
+# x$Season
+# x[1, 'G']
